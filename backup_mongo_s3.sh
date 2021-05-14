e71c8cda83313ba4e79b4738b5bfc22b0d45b9af
Kai <kai@kaiherzig.com>
2021-05-14 15:42:47 +0200

    Updated filter using RegEx, added filter for system DBs

--- backup_mongo_s3.sh
+++ backup_mongo_s3.sh
@@ -1,26 +1,53 @@
 #!/bin/bash
+#MongoDB User
+MongoUser="<<FillMe>>"
+#MongoDB Password
+MongoPassword="<<FillMe>>"
+#AWS S3 Bucket for Backup
+TargetBucket="<<FillMe>>"
+#Prefix for Filename as Date
+FileName=$(date '+%Y-%m-%d_%H-%M-%S')
+#Dir for Exports !! ending with /!!
+Dir=/data/export/
+#Skip admin,config,local DBs
+SkipAdminDBs="true"
 
-user=<<fillme>>
-password=<<fillme>>
-targetbucket=<<fillme>>
-filename=`date '+%Y-%m-%d_%H-%M-%S'`
-dir=/data/export
-
-if [[ ! -e $dir ]]; then
-    mkdir $dir
+#Regex for getting sizes of DBs.
+RegEx="^[^a-z][.0-9]*.[.0-9][.0-9][.0-9]GB"
+#Does export folder exist? If not create it!
+if [[ ! -e $Dir ]]; then
+    mkdir $Dir
 fi
-dbs=$(mongo -u $user -p $password --authenticationDatabase admin --quiet <<EOF
+#Getting DB list from MongoDB
+DBs=$(
+    mongo -u $MongoUser -p $MongoPassword --authenticationDatabase admin --quiet <<EOF
 show dbs
 quit()
 EOF
 )
+
+#Init counter variable
 i=0
-for db in ${dbs[*]}
-do
-    i=$(($i+1))
-    if (($i % 2)); then
-        mongodump --archive=$dir"/"$filename"_"$db --host localhost:27017 --db $db -u $user -p $password --tlsInsecure --authenticationDatabase=admin --gzip
+
+#Loop through all DBs
+for DB in ${DBs[*]}; do
+    i=$(($i + 1))
+    #Filter Sizes of DBs (x.xxxGB)
+    if ! [[ $DB =~ $RegEx ]]; then
+        #Filter not active so export everything.
+        if [ $SkipAdminDBs = "false" ]; then
+            #Exporting all DBs incl. admin and config
+            mongodump --archive=$Dir$FileName"_"$DB --host localhost:27017 --db $DB -u $MongoUser -p $MongoPassword --tlsInsecure --authenticationDatabase=admin --gzip
+
+        elif [ $SkipAdminDBs = "true" ]; then
+            #If SkipAdminDB is set to "true" and DB is neither "admin" nor "config" nor "local"
+            if ! { [ $DB = "config" ] || [ $DB = "admin" ] || [ $DB = "local" ]; }; then
+                mongodump --archive=$Dir$FileName"_"$DB --host localhost:27017 --db $DB -u $MongoUser -p $MongoPassword --tlsInsecure --authenticationDatabase=admin --gzip
+            fi
+        fi
     fi
 done
-aws s3 cp --recursive $dir/ s3://$targetbucket/
-rm -f $dir/*
+#Upload to S3
+aws s3 cp --recursive $Dir/ s3://$TargetBucket/
+#Cleanup the exports
+rm -f $Dir/*
